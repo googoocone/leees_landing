3f1d5ce04ca8e48952234bc6f6ec07c777a44ebb
googoocone1 <googoocone1@naver.com>
2025-03-10 00:04:42 +0900

    영상 용량 줄였음 데스크탑 80% 모바일 99프로

--- components/section1.tsx
+++ components/section1.tsx
@@ -1,8 +1,20 @@
 export default function Section1() {
   return (
     <section className="w-full">
-      <video autoPlay muted loop className="mx-auto">
-        <source src="/landingMv.mp4" type="video/mp4" />
+      <video
+        autoPlay
+        muted
+        loop
+        className="mx-auto"
+        preload="auto"
+        poster="/landing_poster.png"
+      >
+        <source
+          src="/landingMv_mb.mp4"
+          type="video/mp4"
+          media="(max-width: 768px)"
+        />
+        <source src="/landingMv_2.mp4" type="video/mp4" />
         브라우저가 비디오를 지원하지 않습니다.
       </video>
     </section>
