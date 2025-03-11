092fbf424c105eb72995d007d1d236ed31bf1a72
googoocone1 <googoocone1@naver.com>
2025-03-11 12:39:57 +0900

    bottom fixed

--- components/bottom.jsx
+++ components/bottom.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const bottom = () => {
   return (
-    <div className="w-full h-[300px] text-xs px-2 sm:px-30 pt-10  bg-black flex flex-col items-start justify-start text-white">
+    <div className="w-full h-[200px] sm:h-[300px] text-xs px-2 sm:px-30 pt-10  bg-black flex flex-col items-start justify-start text-white">
       <div>상호 : 법무법인 스탠다드</div>
       <div>광고책임 변호사 : 이서영</div>
       <div>전화번호 : 1899-7754</div>
