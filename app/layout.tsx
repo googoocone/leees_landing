6a9c6614b2506c2c5290296bd50ff68d295b77af
googoocone1 <googoocone1@naver.com>
2025-06-12 11:10:07 +0900

    네이버 메타태그

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,12 @@ export const metadata: Metadata = {
   title: "법인 회생, 법인파산 전문 이은성 변호사",
   description:
     "법무법인 스탠다드 이은성 대표변호사, 법인 회생/법인 파산을 전문으로 하며, 변호사 생활동안 오직 법인회생/법인파산만 수임했습니다. ",
+  // 여기에 네이버 사이트 인증 메타 태그를 추가합니다.
+  verification: {
+    other: {
+      "naver-site-verification": "afa3743b0b5e43b0837352689ba4912d06cf307b",
+    },
+  },
 };
 
 export default function RootLayout({
